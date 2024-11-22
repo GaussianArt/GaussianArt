6637fb4ad76b093bda2cf3c1d5645d4f01a5f734
artgs-iclr <artgs-iclr.outlook.com>
2024-11-22 16:08:57 +0800

    fix bug

--- gif/compress.py
+++ gif/compress.py
@@ -3,5 +3,6 @@ import os
 
 gifs = glob('*.gif')
 for gif in gifs:
-    # os.system(f'gifsicle -O3 {gif} --colors 256 -o {gif}')
-    os.system(f'gifsicle --scale 0.5 {gif} -o ../gif1/{gif}')
+    if '11586' in gif:
+        os.system(f'gifsicle -O3 {gif} --colors 256 -o {gif}')
+        # os.system(f'gifsicle --scale 0.5 {gif} -o {gif}')
