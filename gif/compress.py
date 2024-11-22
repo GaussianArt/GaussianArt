126e6992a964dc574dfedcddfafd537b94c9cdae
artgs-iclr <artgs-iclr.outlook.com>
2024-11-22 15:53:55 +0800

    compress

--- gif/compress.py
+++ gif/compress.py
@@ -3,4 +3,5 @@ import os
 
 gifs = glob('*.gif')
 for gif in gifs:
-    os.system(f'gifsicle -O3 {gif} --colors 256 -o {gif}')
+    # os.system(f'gifsicle -O3 {gif} --colors 256 -o {gif}')
+    os.system(f'gifsicle --scale 0.5 {gif} -o ../gif1/{gif}')
